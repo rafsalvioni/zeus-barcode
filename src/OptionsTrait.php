728a4c9e42412f3a737a91dca18421a0cf673155
rafsalvioni <>
2016-06-04 17:18:29 -0300

    setOptions() method and better handling options

--- src/OptionsTrait.php
+++ src/OptionsTrait.php
@@ -47,18 +47,30 @@ trait OptionsTrait
      */
     public function setOption($option, $value)
     {
-        $option = \strtolower($option);
-        if (\array_key_exists($option, $this->options)) {
-            $type = \gettype($this->options[$option]);
-            if (\gettype($value) == $type) {
-                $this->options[$option] = $value;
-                return $this;
-            }
-            throw new Exception("Option \"$option\" should be a $type value");
+        $method = "set$option";
+        if (\method_exists($this, $method)) {
+            $this->$method($value);
         }
-        throw new Exception("Unknown option \"$option\"");
+        else {
+            $this->checkAndSetOption($option, $value);
+        }
+        return $this;
     }
     
+    /**
+     * Set a array of options.
+     * 
+     * @param array $options
+     * @return self
+     */
+    public function setOptions(array $options)
+    {
+        foreach ($options as $option => &$value) {
+            $this->setOption($option, $value);
+        }
+        return $this;
+    }
+
     /**
      * Allows set options using a object property notation.
      * 
@@ -67,13 +79,7 @@ trait OptionsTrait
      */
     public function __set($name, $value)
     {
-        $method = "set$name";
-        if (\method_exists($this, $method)) {
-            $this->$method($value);
-        }
-        else {
-            $this->setOption($name, $value);
-        }
+        $this->setOption($name, $value);
     }
     
     /**
@@ -97,5 +103,27 @@ trait OptionsTrait
     {
         $name = \strtolower($name);
         return $this->getOption($name) !== null;
+    }
+    
+    /**
+     * Check and set a option.
+     * 
+     * @param string $option
+     * @param mixed $value
+     * @return self
+     * @throws Exception
+     */
+    protected function checkAndSetOption($option, $value)
+    {
+        $option = \strtolower($option);
+        if (\array_key_exists($option, $this->options)) {
+            $type = \gettype($this->options[$option]);
+            if (\gettype($value) == $type) {
+                $this->options[$option] = $value;
+                return $this;
+            }
+            throw new Exception("Option \"$option\" should be a $type value");
+        }
+        throw new Exception("Unknown option \"$option\"");
     }
 }
