60f3444ce5d131b605f308e977ed1630d9ded8cc
Rafael M. Salvioni <rafael.salvioni@gmail.com>
2024-06-29 02:53:52 -0300

    PHP7 Update
    PHPUnit update to fix vulnerability
    
    Signed-off-by: Rafael M. Salvioni <rafael.salvioni@gmail.com>

--- test/Ean128Test.php
+++ test/Ean128Test.php
@@ -12,14 +12,13 @@ class Ean128Test extends Code128Test
 {
     /**
      * @test
-     * @depends validateTest
      */
     public function fnc1Test()
     {
         for ($i = 0; $i < 10; $i++) {
             $string = \uniqid();
             $obj = new Ean128($string);
-            $this->assertEquals(\substr($obj->getEncoded()->getBinary(), 11, 11), '11101011110');
+            $this->assertEquals(\substr($obj->getEncoded()->getBinary(), 11, 11), '11110101110');
         }
     }
 }
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,6 +1,13 @@
 <?php
-
 require '../vendor/autoload.php';
 
-//$bc = new \Zeus\Barcode\InterleavedCode25('123456701234567012345670123456701234567', false);
-//echo $bc->render(new Zeus\Barcode\Renderer\HtmlRenderer())->getResource();
+use Zeus\Barcode\Code2of5\Interleaved;
+use Zeus\Barcode\Code2of5\Standard;
+use Zeus\Barcode\Upc\Ean13;
+use Zeus\Barcode\Renderer\SvgRenderer;
+
+$renderer = new SvgRenderer();
+
+$bc = new Ean13('938746850912', false);
+$bc->scale(5);
+$bc->draw($renderer)->render(); // Show the result
